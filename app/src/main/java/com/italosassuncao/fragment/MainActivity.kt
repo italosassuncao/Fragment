b5c971da2be01954ac087c6791702e6c34e01142
Italo Silva de Assunção <83298919+italosassuncao@users.noreply.github.com>
2025-05-12 21:27:23 -0300

    feat: Add Chamadas Fragment and button to switch fragments
    This commit introduces a new Fragment called `ChamadasFragment` and its corresponding layout `fragment_chamadas.xml`. It also adds functionality to switch between the `ConversasFragment` and the new `ChamadasFragment` using buttons in the `MainActivity`.
    
    Key changes:
    - Added `ChamadasFragment.kt` to represent the calls screen.
    - Added `fragment_chamadas.xml` as the layout for `ChamadasFragment`.
    - Modified `activity_main.xml` to rename the existing buttons to `button_conversas` and `button_chamadas` and updated the `FragmentContainerView` id to `fragment_content`.
    - Updated `MainActivity.kt` to implement click listeners for the buttons to replace the content of `fragment_content` with either `ConversasFragment` or `ChamadasFragment`.

--- app/src/main/java/com/italosassuncao/fragment/MainActivity.kt
+++ app/src/main/java/com/italosassuncao/fragment/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.italosassuncao.fragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.italosassuncao.fragment.fragments.ChamadasFragment
+import com.italosassuncao.fragment.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonConversas: Button
+    private lateinit var buttonChamadas: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        buttonConversas = findViewById(R.id.button_conversas)
+        buttonChamadas = findViewById(R.id.button_chamadas)
+
+        val fragment = supportFragmentManager.beginTransaction()
+        /*fragment.add(R.id.fragment_content, ConversasFragment())
+        fragment.commit()*/
+
+        // Alternativa
+        buttonConversas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_content, ConversasFragment())
+                .commit()
+        }
+
+        buttonChamadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_content, ChamadasFragment())
+                .commit()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
